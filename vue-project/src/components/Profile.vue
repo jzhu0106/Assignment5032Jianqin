38e9d192f138bd94b4139ea550d366870174cf54
zhujianqin <jzhu0106@student.Monash.edu>
2025-08-27 16:26:03 +1000

    edit profile

--- vue-project/src/components/Profile.vue
+++ vue-project/src/components/Profile.vue
@@ -1,49 +1,86 @@
 <template>
   <div class="container">
-    <h1>{{ profile[0].name }}</h1>
-    <ul>
-      
-      <li>Email: {{ profile[0].email }}</li>
-      <li>Birth Date: {{ profile[0].birthDate }}</li>
-      <li>Gender: {{ profile[0].gender }}</li>
-      <li>Address: {{ profile[0].address }}</li>
-      <li>Occupation: {{ profile[0].occupation }}</li>
-      <li>Favorite Desserts: 
-        <ul>
-          <li v-for="dessert in profile[0].favoriteDesserts" :key="dessert">{{ dessert }}</li>
-        </ul>
-      </li>
-      <li>Bio: {{ profile[0].bio }}</li>
-    </ul>
+    <p>User ID: {{ userProfile.userId }}</p>
+    
+    <p>
+      Email: 
+      <input v-if="isEditing" v-model="userProfile.email" /> 
+      <span v-else>{{ userProfile.email }}</span>
+    </p>
+    
+    <p>
+      Birth Date: 
+      <input v-if="isEditing" v-model="userProfile.birthDate" type="date" /> 
+      <span v-else>{{ userProfile.birthDate }}</span>
+    </p>
+    
+    <p>
+      Gender: 
+      <select v-if="isEditing" v-model="userProfile.gender">
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+      <span v-else>{{ userProfile.gender }}</span>
+    </p>
 
-    <button> Edit </button>
-    <button> Save </button>
+    <p>
+      Address: 
+      <input v-if="isEditing" v-model="userProfile.address" /> 
+      <span v-else>{{ userProfile.address }}</span>
+    </p>
+    
+    <p>
+      Occupation: 
+      <input v-if="isEditing" v-model="userProfile.occupation" /> 
+      <span v-else>{{ userProfile.occupation }}</span>
+    </p>
+
+    <p>Favorite Desserts:
+      <ul>
+        <li v-for="(dessert, index) in userProfile.favoriteDesserts" :key="index">
+          <input v-if="isEditing" v-model="userProfile.favoriteDesserts[index]" />
+          <span v-else>{{ dessert }}</span>
+        </li>
+      </ul>
+    </p>
+    
+    <p>
+      Bio: 
+      <textarea v-if="isEditing" v-model="userProfile.bio"></textarea>
+      <span v-else>{{ userProfile.bio }}</span>
+    </p>
+    <button @click="editProfile" v-if="!isEditing">Edit</button>
+    <button @click="saveProfile" v-if="isEditing">Save</button>
   </div>
 </template>
 
-
-
 <script setup>
 import profile from "../assets/json/UserProfile.json";
+import { ref, computed } from "vue";
 
-import { ref, computed } from "vue"
 
-// Our logic will go here
+const userProfile = ref(profile[0]);
+const isEditing = ref(false);
+
+
+const editProfile = () => {
+  isEditing.value = true;
+};
+
+
+const saveProfile = () => {
+  isEditing.value = false;
+};
 </script>
 
 <style scoped>
-/* h1 {
-  margin-bottom: 20px;
-} */
-
-
- .container {
+.container {
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   max-width: 80vw;
   margin: 0 auto;
   padding: 20px;
-  /* background-color: #f4f4f4; */
-  /* border-radius: 10px; */
-  /* box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); */
+  background-color: #f4f4f4;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
