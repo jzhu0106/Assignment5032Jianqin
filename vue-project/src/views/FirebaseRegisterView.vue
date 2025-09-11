8e2175a5393fa32bf803754080385072bb4422de
zhujianqin <jzhu0106@student.Monash.edu>
2025-09-12 03:35:17 +1000

    register add validation and UI enhancemen-Bootstrap

--- vue-project/src/views/FirebaseRegisterView.vue
+++ vue-project/src/views/FirebaseRegisterView.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Register Page</h1>
-  <h2>Create an Account</h2>
-  <p><input type="text" placeholder="Email" v-model="email" /></p>
-  <p><input type="password" placeholder="Password" v-model="password" /></p>
-  <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
-  <p><button @click="register">Register</button></p>
+  <div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-6 col-lg-4">
+  <h1>Create an Account</h1>
+  <div class="mb-3">
+  <label class="form-label">Email</label>
+  <input type="text" class="form-control" placeholder="Email" v-model="formData.email" />
+</div>
+<div v-if="errors.email" class="text-danger">{{ errors.email }}</div>
+  <div class="mb-3">
+  <label class="form-label">Password</label>
+  <input type="password" class="form-control" placeholder="Password" v-model="formData.password" />
+</div>
+<div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+  <div v-if="errorMessage" class="text-danger">{{ errorMessage }}</div>
+  <button class="btn btn-primary w-100" @click="register">Register</button>
+   </div>
+  </div>
+</div>
 </template>
 
 <script setup>
@@ -15,28 +28,70 @@ import { useRouter } from "vue-router";
 import { collection, addDoc } from 'firebase/firestore';
 
 
-const email = ref("");
-const password = ref("");
+const formData = ref({
+  email: '',
+  password: ''
+});
 const errorMessage = ref("");
 const router = useRouter();
 const auth = getAuth();
 
-const register = () => {
-  if (!email.value || !password.value) {
-    errorMessage.value = 'please enter the email and password';
-    return;
+
+const errors = ref({
+  email: null,
+  password: null,
+});
+
+const validateEmail = () => {
+  if (!formData.value.email) {
+    errors.value.email = "Email is required";
+  } else {
+    errors.value.email = null;
   }
+};
+
+const validatePassword = () => {
+  const password = formData.value.password;
+  const minLength = 8;
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+={}\[\]:;"'<>,.?\/\\|]/.test(password);
+
+  if (password.length < minLength) {
+    errors.value.password = `Password must be at least ${minLength} characters long.`;
+  } else if (!hasUppercase) {
+     errors.value.password = "Password must contain at least one uppercase letter.";
+  } else if (!hasLowercase) {
+     errors.value.password = "Password must contain at least one lowercase letter.";
+  } else if (!hasNumber) {
+     errors.value.password = "Password must contain at least one number.";
+  } else if (!hasSpecialChar) {
+    errors.value.password = "Password must contain at least one special character.";
+  } else {
+    errors.value.password = null;
+  }
+};
+
+
+const register = () => {
+validateEmail();
+validatePassword();
+
+if (errors.value.email || errors.value.password) {
+  return;
+}
 
   errorMessage.value = "";
 
-  createUserWithEmailAndPassword(auth, email.value, password.value)
+  createUserWithEmailAndPassword(auth, formData.value.email, formData.value.password)
     .then(async (data) => {
       console.log("Firebase Register Successful!");
 
 
     //create user doc
     await addDoc(collection(db, 'users'), {
-      email: email.value,
+      email: formData.value.email,
       role: 'user', //default as user
     });
 
