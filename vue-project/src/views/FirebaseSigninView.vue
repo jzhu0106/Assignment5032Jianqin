cbf3dcaed12a273b1c9856baea6ca9c880d73451
zhujianqin <jzhu0106@student.Monash.edu>
2025-09-12 02:47:35 +1000

    Sign-in page UI enhancemen-Bootstrap

--- vue-project/src/views/FirebaseSigninView.vue
+++ vue-project/src/views/FirebaseSigninView.vue
@@ -1,10 +1,28 @@
 <template>
+  <div class="container mt-5">
+  <div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-6 col-lg-4">
   <h1>Sign in</h1>
-  <p><input type="text" placeholder="Email" v-model="email" /></p>
-  <p><input type="password" placeholder="Password" v-model="password" /></p>
-  <p v-if="errorMessage" style="color: red;">{{ errorMessage }}</p>
-  <p><button @click="signin">Sign in</button></p>
-  <p><button @click="goToRegister">Register</button></p>
+  <div class="mb-3">
+  <label class="form-label">Email</label>
+  <input type="text" class="form-control" placeholder="Email" v-model="email" />
+</div>
+  <div v-if="errors.email" class="text-danger">{{ errors.email }}</div>
+  <div class="mb-3">
+  <label class="form-label">Password</label>
+  <input type="password" class="form-control" placeholder="Password" v-model="password" />
+</div>
+  <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+  <div v-if="errorMessage" class="text-danger">{{ errorMessage }}</div>
+<div class="d-flex gap-2">
+<button class="btn btn-primary" @click="signin">Sign in</button>
+<button class="btn btn-primary" @click="goToRegister">Register</button>
+</div>
+      </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script setup>
@@ -17,12 +35,38 @@ const password = ref("");
 const router = useRouter();
 const auth = getAuth();
 
+
+
+const errors = ref({
+  email: null,
+  password: null,
+});
+
+const validateEmail = () => {
+  if (!email.value) {
+    errors.value.email = "Email is required";
+  } else {
+    errors.value.email = null;
+  }
+};
+
+const validatePassword = () => {
+  if (!password.value) {
+    errors.value.password = "Password is required";
+  } else {
+    errors.value.password = null;
+  }
+};
+
 const signin = () => {
   errorMessage.value = "";
 
-    if (!email.value || !password.value) {
-    alert('please enter the email and password');
-    return;}
+validateEmail();
+validatePassword();
+
+if (errors.value.email || errors.value.password) {
+  return;
+}
   signInWithEmailAndPassword(auth, email.value, password.value)
     .then((data) => {
       console.log("Firebase Register Successful!");
